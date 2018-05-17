e1bc5607b78314aa17902e1fb1a9a57da4a9b9f2
Michael Bench <bench.michael@gmail.com>
2018-05-17 08:16:29 -0600

    Not sure if this is correct but i tried

---
+++ Renderer/Models/HtmlRenderer/Components/HtmlImageRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Factory.Components;
+using Factory.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Renderer.Models.HtmlRenderer.Components
+{
+    public class HtmlImageRenderer : ComponentRenderer<Image, HtmlRenderOut>
+    {
+        public HtmlImageRenderer(Image RendererDataObject) : base(RendererDataObject) { }
+
+        public override void Render(HtmlRenderOut writer, ComponentRenderer<HtmlRenderOut> parent = null)
+        {
+            writer.WriteLine("View", $"<a href=\"{_RendererDataObject.LinkPath}\">");
+            writer["View"].Indent++;
+
+            writer.WriteLine("View", $"<img src=\"{_RendererDataObject.LinkPath}\">");
+            writer["View"].Indent++;
+            RenderChildren(writer);
+            writer["View"].Indent--;
+            writer.WriteLine("View", $"</img>");
+
+            writer["View"].Indent--;
+            writer.WriteLine("View", $"</a>");
+        }
+    }
+
+}
